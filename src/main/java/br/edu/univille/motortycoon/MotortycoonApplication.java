ca4a23007954336be9c90cd226a8bbdcc6835b3a
murilott <murilo_teichert@hotmail.com>
2025-05-05 16:38:23 -0300

    first commit

---
+++ src/main/java/br/edu/univille/motortycoon/MotortycoonApplication.java
@@ -0,0 +1,13 @@
+package br.edu.univille.motortycoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotortycoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotortycoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/univille/motortycoon/MotortycoonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.univille.motortycoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotortycoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
